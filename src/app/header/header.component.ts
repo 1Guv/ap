e811eeaf95b8116d280d2ac02f81e500f2c982f7
Gurvinder <gurvinder.singh.sandhu@gmail.com>
2019-03-07 15:14:53 +0000

    updated banner text
    scrolls through the text in the array smoothly

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -15,11 +15,12 @@ export class HeaderComponent implements OnInit {
   isMobileResolution: boolean;
   // isTabletResolution: boolean;
 
+  // Initial banner message
   headerBannerMessage = 'Advertise your Private ASIAN Number Plate here...';
 
   subscription: Subscription;
   intervalId: number;
-  counter = 1;
+  counter = 0;
 
   constructor() { }
 
@@ -50,17 +51,13 @@ export class HeaderComponent implements OnInit {
     pixelResolution <= 810 ? this.isMobileResolution = true : this.isMobileResolution = false;
   }
 
-  displayBannerHeaderText(text: string, counter) {
-    if (this.counter === text.length) {
+  displayBannerHeaderText(text: string[], counter: number) {
+    if (this.counter < text.length) {
+      // console.log('text', text[this.counter]);
+      this.counter++;
+      this.headerBannerMessage = text[counter];
+    } else {
       this.counter = 0;
     }
-    console.log('displaybannerHeaderText');
-    this.counter++;
-    this.headerBannerMessage = text[counter];
-  }
-
-  // tslint:disable-next-line:use-life-cycle-interface
-  ngOnDestroy() {
-    this.subscription && this.subscription.unsubscribe();
   }
 }
