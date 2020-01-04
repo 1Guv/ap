d469e76db928703909df34f73422adb220efdeab
Guv <Gurvinder.Singh.Sandhu@gmail.com>
2020-01-04 16:56:47 +0000

    upgraded to ng9

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,9 @@ import { AppRoutingModule } from './app-routing/app-routing.module';
 import { HeaderComponent } from './header/header.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppMaterialModule } from './app-material/app-material.module';
-import { MatIconModule } from '@angular/material';
+import { MatIconModule } from '@angular/material/icon';
 import { MainComponent } from './main/main.component';
-import { MatSidenavModule } from '@angular/material';
+import { MatSidenavModule } from '@angular/material/sidenav';
 
 @NgModule({
   declarations: [
