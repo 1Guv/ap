c73607d69153989fd68fb1b009feec79c538b62e
Guv <Guv@thehouseshop.com>
2020-05-17 22:35:11 +0100

    created component for menu and repeated over options

--- src/app/_elements/header-menu/header-menu.component.ts
+++ src/app/_elements/header-menu/header-menu.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { MenuOptions } from '../../_models/menu-options';
 
 @Component({
   selector: 'app-header-menu',
@@ -9,6 +10,37 @@ export class HeaderMenuComponent implements OnInit {
 
   // Getting sidenav element from parent so we can use sidenav.toggle()
   @Input() sidenav: any;
+
+  arrayOfMenuOptions: MenuOptions[] = [
+    {
+      optionName: 'List Your Plate',
+      optionLink: '',
+    },
+    {
+      optionName: 'How It Works',
+      optionLink: '',
+    },
+    {
+      optionName: 'Get a Valuation',
+      optionLink: '',
+    },
+    {
+      optionName: 'Price',
+      optionLink: '',
+    },
+    {
+      optionName: 'About',
+      optionLink: '',
+    },
+    {
+      optionName: 'Contact',
+      optionLink: '',
+    },
+    {
+      optionName: 'Login',
+      optionLink: '',
+    }
+  ]
 
   constructor() { }
 
---
+++ src/app/_models/menu-options.ts
@@ -0,0 +1,4 @@
+export class MenuOptions {
+    optionName: string;
+    optionLink: string;
+}
